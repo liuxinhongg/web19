671d5ca6b4738ee5a6d917b287758258d9046e0b
liuxinhongg <644391351@qq.com>
2020-10-12 15:07:43 +0800

    vue-router

---
+++ vue/vue-test/src/components/work.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="jump()">我是work组件</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+      jump(){
+        // this.$router.push("/page?aa=xiaoming")
+        this.$router.push({name:"page",params:{aa:"xiaoming"}})
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
--- vue/vue-test/src/router/index.js
+++ vue/vue-test/src/router/index.js
@@ -3,7 +3,19 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Frist from '@/components/first'
 import page from '@/components/page'
-
+import work from '@/components/work'
+const User={
+  template:` <div><h1>我是父组件{{$route.params.id}}</h1><router-view/></div> `
+}
+const profile={
+  template:` <div><h2> <font color="red">我是profile子组件</font> </h2> </div> `
+}
+const posts={
+  template:` <div><h2> <font color="yellowgreen">我是posts子组件</font> </h2> </div> `
+}
+const NotFount={
+  template:`<div><h1>404您访问的页面不存在</h1></div>`
+}
 Vue.use(Router)
 
 export default new Router({
@@ -24,6 +36,40 @@ export default new Router({
       path:"/page",
       name:"page",
       component:page
-    }
+    },
+    {
+      path:"/work",
+      name:"work",
+      component:work
+    },
+    {
+      path:"/user/:id",
+      name:"user",
+      component:User,
+      children:[
+        {
+          path:"",
+          component:profile
+        },{
+          // path:"posts",
+          path:"/posts",
+          component:posts
+        }
+      ]
+    },
+    {
+      path:"*",
+      // component:NotFount,
+      redirect:(to)=>{
+        console.log(to);
+        if(to.path==='/aaa'){
+          return '/page'
+        }else if(to.path==='/bbb'){
+          return "/first/abc"
+        }else{
+          return "/"
+        }
+      }
+     }
   ]
 })
